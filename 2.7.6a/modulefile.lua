83d6729fcd4dcccd1d4edba35d9c9dd61777e3c1
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2021-01-04 08:23:22 -0500

    :truck: Renamed STAR
    Renamed star to STAR

--- 2.7.6a/modulefile.lua
+++ 2.7.6a/modulefile.lua
@@ -1,36 +1,37 @@
 --
--- star 2.7.6a modulefile
+-- STAR 2.7.6a modulefile
 --
 -- "URL: https://www.psc.edu/resources/software"
 -- "Category: Biological Sciences"
 -- "Description: Spliced Transcripts Alignment to a Reference (STAR) is a fast RNA-seq read mapper, with support for splice-junction and fusion read detection."
 -- "Keywords: singularity bioinformatics"
 
-whatis("Name: star")
+whatis("Name: STAR")
 whatis("Version: 2.7.6a")
 whatis("Category: Biological Sciences")
 whatis("URL: https://www.psc.edu/resources/software")
 whatis("Description: Spliced Transcripts Alignment to a Reference (STAR) is a fast RNA-seq read mapper, with support for splice-junction and fusion read detection.")
 
 help([[
-star 2.7.6a
+STAR 2.7.6a
 -----------
 
 Description
 -----------
 Spliced Transcripts Alignment to a Reference (STAR) is a fast RNA-seq read mapper, with support for splice-junction and fusion read detection.
 
-To load the module type
+To load the module, type
 
-> module load star/2.7.6a
+> module load STAR/2.7.6a
 
-To unload the module type
+To unload the module, type
 
-> module unload star/2.7.6a
+> module unload STAR/2.7.6a
 
 Documentation
 -------------
 https://github.com/alexdobin/STAR/blob/master/doc/STARmanual.pdf
+
 For help, type
 
 > STAR -help
@@ -44,7 +45,7 @@ Tools included in this module are
 * STAR
 ]])
 
-local package = "star"
+local package = "STAR"
 local version = "2.7.6a"
 local base    = pathJoin("/opt/packages",package,version)
 prepend_path("PATH", base)
